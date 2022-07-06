5d53dc31eeff6b0f57d98c8ad3524964f4aeb8eb
costikluciu2020 <costikluciu2020@gmail.com>
2022-07-06 15:44:17 +0100

    I am able to loop through the boxes

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,39 @@
 /*
+Mouse over a div
+Method1:
+document.getElementById("element").addEventListener("mouseenter", function(  ) {isOnDiv=true;});
+document.getElementById("element").addEventListener("mouseout", function(  ) {isOnDiv=false;});
+
+Method2
+ <img onmouseover="bigImg(this)" src="smiley.gif" alt="Smiley">
+
 
 
 */
+
+//console.log("fooo");
+let matrix = document.querySelector(".matrix");
+//console.log("matrix=",matrix);
+
+let boxesArray = document.querySelectorAll(".box");
+console.log("box=",boxesArray);
+
+// Add the code that detexts when the mouse cursor is over
+// First I need to interate over all the boxes from `boxesArray`
+//box.addEventListener("mouseover", changeColor);
+
+for (let box of boxesArray){
+	//DEBUG console.log(box);
+	//box.addEventListener("mouseover", changeColor, eventObj.target);
+	box.addEventListener("mouseover", changeColor);
+}
+
+// Define the changeColor() function
+function changeColor(box){
+	//console.log("test");
+	console.log(box);
+	//box.style.backgroundColor="white";
+}
+
 
 
